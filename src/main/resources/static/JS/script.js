927aaf5d50bcb9b699fb89430b304e0cb2df2245
Nothing2003 <soumojitmakar1234@gmail.com>
2024-08-24 19:17:13 +0530

    Add Dark and Light theme

---
+++ src/main/resources/static/JS/script.js
@@ -0,0 +1,33 @@
+
+function setTheme(theme){
+    localStorage.setItem("theme",theme);
+}
+function getTheme(){
+  let theme=localStorage.getItem("theme");
+ return theme? theme: "light";
+}
+
+let current=getTheme();
+document.addEventListener("DOMContentLoaded",()=>{
+    changeTheme(current);
+});
+
+function changeTheme(){
+document.querySelector('html').classList.add(current);
+const change= document.querySelector('#theme_change');
+change.addEventListener("click",()=>{console.log("change")
+   const old=current;
+    if(current=='dark'){
+        current='light'
+    }else{
+        current='dark'
+    }
+    setTheme(current);
+    document.querySelector('html').classList.remove(old);
+    document.querySelector('html').classList.add(current);
+    const text=current=='dark'? "Light":"Dark";
+    change.querySelector('span').textContent=text;
+    
+});
+
+}
