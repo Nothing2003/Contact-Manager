b8b9989331ab641d8435fa042b74f3113bf094e8
Nothing2003 <soumojitmakar1234@gmail.com>
2024-08-24 17:23:38 +0530

    add some new view

--- src/main/java/com/cm/cm2/controller/PageController.java
+++ src/main/java/com/cm/cm2/controller/PageController.java
@@ -7,21 +7,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/home")
+    @GetMapping(value = "/home")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/about")
+    @GetMapping(value = "/about")
     public String aboutPage(Model model) {
-         model.addAttribute("isLogin",false);
+        model.addAttribute("isLogin", false);
 
         return "about";
     }
 
-    @GetMapping("/services")
+    @GetMapping(value = "/services")
     public String servicesPage() {
         return "services";
+    }
+
+    @GetMapping(value = "/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping(value = "/register")
+    public String registerPage() {
+        return "register";
     }
 
 }
